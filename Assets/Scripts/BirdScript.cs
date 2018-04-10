b1b900d6a80036d8af67e1c2e9017f0124543cbc
Antonio <reytono@gmail.com>
2018-04-10 08:33:44 -0600

    Compltar hasta 2.11

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    public float jumpForce = 200f;
+    Rigidbody2D rb;
+
+    public float forwardSpeed = 2f;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+    }
+
+
+}
